6c09d7c5e07d1bf426dd9a5044338fb9b0b6278f
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-05-24 20:45:37 +0600

    home page ui updated and added header and charjs with animated countup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>home</div>;
+  const loggedIn = { firstName: "Rakib" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and mange your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.51}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from "react";
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp
+        end={amount}
+        decimal="."
+        separator=","
+        duration={2.75}
+        decimals={2}
+        prefix="$"
+      />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
