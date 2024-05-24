3141a5712ed9966a16add54d069c855e5584d8be
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-05-24 22:27:06 +0600

    side bar desktop design added

---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+
+const Sidebar = ({ user }: SidebarProps) => {
+  const pathname = usePathname();
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link
+          href="/"
+          className="mb-12 cursor-pointer items-center gap-2 flex justify-start"
+        >
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Side bar logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">TD Bank</h1>
+        </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+        USER
+      </nav>
+      Footer
+    </section>
+  );
+};
+
+export default Sidebar;
